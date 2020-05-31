c5ca41ae08fd16750571bf82160da22d1ec12ccc
Irina Hristova <hristova119@gmail.com>
2020-05-31 10:26:28 +0300

    chore: cyk command

--- oop-context-free-grammar/context-free-grammar/CykCommand.cpp
+++ oop-context-free-grammar/context-free-grammar/CykCommand.cpp
@@ -1,8 +1,30 @@
 #include "CykCommand.h"
+#include "Validator.h"
+#include <map>
 
-std::string CykCommand::execute(const std::vector<std::string>&)
+CykCommand::CykCommand(Store* store)
 {
-	return std::string();
+	ICommand::store = store;
+}
+
+std::string CykCommand::execute(const std::vector<std::string>& parameters)
+{
+	if (Validator::isValidParametersCount(3, parameters.size()))
+	{
+		int id = std::stoi(parameters[1]);
+		std::string word = parameters[2];
+		if (Validator::isValidGrammarId(id, this->store->getGrammars()))
+		{
+			Grammar* g = this->store->findGrammarById(id);
+			if (!g->chomsky()) {
+				return Constants::CykNotPossible;
+			}
+			
+			return Constants::ToImplement;
+		}
+		else return Constants::NoGrammar;
+	}
+	else return Constants::InvalidParameters;
 }
 
 std::string CykCommand::toString()
--- oop-context-free-grammar/context-free-grammar/CykCommand.h
+++ oop-context-free-grammar/context-free-grammar/CykCommand.h
@@ -9,6 +9,8 @@
 class CykCommand : public ICommand
 {
 public:
+	CykCommand(Store*);
+
 	virtual std::string execute(const std::vector<std::string>&) override;
 	virtual std::string toString() override;
 };
